e4945354bdf85aafa08dcb7404484c443617d8ee
Jonny Ortiz <jonnyortiz@Jonnys-MBP.home>
2023-05-24 01:02:10 -0400

    add base users api

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const app = express()
+const port = 3001
+const users = []
+
+app.use(express.json())
+
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
+app.post('/users', async (req, res) => {
+    try {
+        let password = await bcrypt.hash(req.body.password, 10)
+        const user = { email: req.body.email, password }
+        users.push(user)
+        res.status(201).send()
+    } catch (error) {
+        
+    }
+})
+
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.email === req.body.email)
+    if (!user) {
+        return res.status(404).send('Cannot find user')
+    }
+    try {
+        if(await bcrypt.compare(req.body.password, user.password)) {
+            res.status(200).send('Success')
+        } else {
+            res.status(401).send('Unauthorized')
+        }
+    } catch (error) {
+        res.status(500).send('Oops, something happened')
+    }
+})
+
+app.listen(port, () => {
+    console.log(`vanilla api is listening on port ${port}`)
+})
